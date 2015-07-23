9a75b28205abd4f0c20abc80e6127fb9c6c1f8c9
Hans Åge Martinsen <hamartin@moshwire.com>
2015-07-23 16:26:36 +0200

    Reviewed the code some more and created a function seeother that is used to hook into methods in webpy classes.

--- vaktplan.py
+++ vaktplan.py
@@ -3,8 +3,14 @@
 
 
 '''
-Vaktplan is a calendar meant to be used by me privately. It supports
-basic authentication, adding and removing comments on specific dates.
+Vaktplan is a calendar meant to be used by me privately. I have
+uploaded it to Github and released it under the BSD license simply
+because maybe, someone would like to use it them selves or use it as a
+starter point to learn python/web.py
+
+Vaktplan support basic loggin in/logging out. The passwords are never
+stored in clear but as sha256 hex digest in a database. It is possible
+to change password after logging in.
 
 Syntax to create the database needed to run the application.
 
@@ -31,7 +37,6 @@ import hashlib
 import web
 
 from sqlite3 import OperationalError
-from web import form
 
 
 #
@@ -106,7 +111,6 @@ def gethash(inputstring):
 
 def updatepassword(username, password):
     ''' Updates a users password in the database. '''
-
     dbh = web.database(dbn=DBTYPE, db=DBFILENAME)
     trans = dbh.transaction()
     try:
@@ -117,6 +121,16 @@ def updatepassword(username, password):
         raise web.internalerror()
     else:
         trans.commit()
+
+
+def seeother(self):
+    ''' Function intended to be hooked into classes where GET and/or
+    POST functions are not needed. The function will forward the user
+    to / or /login depending on if the user is logged in or not. '''
+    if loggedin():
+        raise web.seeother('/')
+    else:
+        raise web.seeother('/login')
 
 
 #
@@ -144,23 +158,15 @@ class Index:
     def __init__(self):
         self.year = int(datetime.date.today().year)
         self.month = int(datetime.date.today().month) - 1
-        self.day = int(datetime.date.today().day)
+        self.POST = seeother
 
     def __str__(self):
-        return repr("Index: {0}.{1}.{2}".format(self.day, self.month,
-                                                                self.year))
+        return repr("Index: {0}.{1}".format(self.month, self.year))
 
     def GET(self):
         ''' Returns the index page. '''
         if loggedin():
             return RENDER.index(MONTHS, self.year, self.month)
-        else:
-            raise web.seeother('/login')
-
-    def POST(self):
-        ''' Dummy, forwards the user to /. '''
-        if loggedin():
-            raise web.seeother('/')
         else:
             raise web.seeother('/login')
 
@@ -170,15 +176,15 @@ class Ym:
     ''' Shows the "month" page. '''
 
     def __init__(self):
-
         i = web.input()
         cal = calendar.Calendar(0)
+        self.POST = seeother
 
         try:
             self.year = int(i.year)
             self.month = int(i.month)
         except:
-            raise web.seeother('/')
+            raise web.internalerror()
         else:
             if(self.year < 1990 or self.year > 2020 or self.month < 0 or
                                                             self.month > 11):
@@ -187,7 +193,6 @@ class Ym:
         self.yearin = int(datetime.date.today().year)
         self.monthin = int(datetime.date.today().month)
         self.dayin = int(datetime.date.today().day)
-
         self.dateslist = cal.monthdays2calendar(self.year, self.month + 1)
 
     def __str__(self):
@@ -201,21 +206,14 @@ class Ym:
             return RENDER.ym(MONTHS, self.year, self.month, self.dateslist,
                                                     self.dayin, self.monthin)
 
-    def POST(self):
-        ''' Dummy, forwards the user to /. '''
-        if loggedin():
-            raise web.seeother('/')
-        else:
-            raise web.seeother('/login')
-
 
 class Day:
 
     ''' Shows the "day" page. '''
 
     def __init__(self):
-
         i = web.input()
+        self.POST = seeother
 
         try:
             self.year = int(i.year)
@@ -248,21 +246,15 @@ class Day:
             return RENDER.day(MONTHS, DAYS, self.year, self.month, self.day,
                     self.weekday, rows)
 
-    def POST(self):
-        ''' Dummy, forwards the user to /. '''
-        if loggedin():
-            raise web.seeother('/')
-        else:
-            raise web.seeother('/login')
-
 
 class Add:
 
     ''' Adds data from page to database. '''
 
     def __init__(self):
-
         i = web.input()
+        self.GET = seeother
+
         try:
             self.year = int(i.year)
             self.month = int(i.month)
@@ -285,13 +277,6 @@ class Add:
     def __str__(self):
         return repr("Add: {0}.{1}.{2}".format(self.day, self.month, self.year))
 
-    def GET(self):
-        ''' Dummy, forwards to /. '''
-        if loggedin():
-            raise web.seeother('/')
-        else:
-            raise web.seeother('/login')
-
     def POST(self):
         ''' Stores data to the database and sends the user back to the
         same page. '''
@@ -308,7 +293,6 @@ class Add:
                 raise
             else:
                 trans.commit()
-            finally:
                 raise web.seeother('/ym/d/?year={0}&month={1}&day={2}'.format(
                                             self.year, self.month, self.day))
 
@@ -318,8 +302,9 @@ class Del:
     ''' Deletes data from page to database. '''
 
     def __init__(self):
-
         i = web.input()
+        self.GET = seeother
+
         try:
             self.rowid = int(i.id)
             self.year = int(i.year)
@@ -336,13 +321,6 @@ class Del:
 
     def __str__(self):
         return repr("Del: {0}.{1}.{2}".format(self.day, self.month, self.year))
-
-    def GET(self):
-        ''' Dummy, forwards to /. '''
-        if loggedin():
-            raise web.seeother('/')
-        else:
-            raise web.seeother('/login')
 
     def POST(self):
         ''' Deletes data from the database and sends the user back to
@@ -369,11 +347,12 @@ class Login:
     ''' Class to deal with all things authentication and login pages. '''
 
     def __init__(self):
-        self.login = form.Form(
-                form.Textbox('username', form.notnull, description='Username'),
-                form.Password('password', form.notnull,
+        self.login = web.form.Form(
+                web.form.Textbox('username', web.form.notnull,
+                                                    description='Username'),
+                web.form.Password('password', web.form.notnull,
                                                     description='Password'),
-                form.Button('Login'))
+                web.form.Button('Login'))
 
     def GET(self):
         ''' Shows the login page. '''
@@ -382,14 +361,13 @@ class Login:
 
     def POST(self):
         ''' Process the login credentials. '''
-
         login = self.login()
+
         if not login.validates():
             return RENDER.login(login)
         else:
             username = login['username'].value
-            password = login['password'].value
-            hpassword = gethash(password)
+            hpassword = gethash(login['password'].value)
 
         try:
             dbh = web.database(dbn=DBTYPE, db=DBFILENAME)
@@ -413,9 +391,6 @@ class Logout:
 
     ''' Class that handles logging users out of the system. '''
 
-    def __init__(self):
-        pass
-
     def GET(self):
         ''' Logs the user out of this session. '''
         SESSION.kill()
@@ -432,15 +407,15 @@ class Changepass:
     ''' A class that handles changing a users password. '''
 
     def __init__(self):
-        self.chpform = form.Form(
-                form.Password('oldpassword', form.notnull,
-                            description='Old password'),
-                form.Password('newpassword', form.notnull,
-                            description='New password'),
-                form.Password('newpassword2', form.notnull,
-                            description='Confirm password'),
-                form.Button('Confirm'),
-                validators=[form.Validator("Passwords didn't match.",
+        self.chpform = web.form.Form(
+                web.form.Password('oldpassword', web.form.notnull,
+                                                description='Old password'),
+                web.form.Password('newpassword', web.form.notnull,
+                                                description='New password'),
+                web.form.Password('newpassword2', web.form.notnull,
+                                            description='Confirm password'),
+                web.form.Button('Confirm'),
+                validators=[web.form.Validator("Passwords didn't match.",
                                 lambda i: i.newpassword == i.newpassword2)])
 
     def GET(self):
@@ -486,6 +461,7 @@ class Changepass:
 #
 # BEGIN
 #
+
 
 APP.notfound = notfound
 application = APP.wsgifunc()
